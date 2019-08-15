5c747fa7b7515d6090352c78e181b9a8ac244959
ajdinl <ajdinl>
2019-08-15 10:19:30 +0200

    img upd

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,13 @@
 import React from 'react'
+import myimage from  '../myimg.jpg'
 import Cv from './Cv';
 
 const Home = () => {
   return (
     <div className='container'>
+      <div className='float-right mr-4 mt-2'>
+        <img src={myimage} alt='myImage' className='rounded border'/>
+      </div>
       <div className='d-flex justify-content-center p-4 mt-3'>
         <h3>Ajdin Lojić</h3>
       </div>
