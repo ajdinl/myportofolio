16f3142e5eeec53f2bdb922bbd2b7ec41c9e769b
ajdinl <ajdinl@live.com>
2020-04-11 09:59:21 +0200

    new cv updated

--- src/components/Cv.js
+++ src/components/Cv.js
@@ -9,149 +9,107 @@ const Cv = () => {
       <div className='d-flex justify-content-center'>
         <div className='text-left w-75'>
           <div className='mb-2'>
-            <b>PERSONAL INFORMATIONS</b><br/>
+            <b>PERSONAL INFORMATION</b><br/>
           </div>
           <div className='mb-3'>
-            First and last name: Ajdin Lojić <br/>
-            Address: Zagrebačka 55a, 71000 Sarajevo, Bosna i Hercegovina <br/>
-            Phone number: 061/811-375 <br/>
-            E-mail: <a href="mailto:ajdinl@live.com">ajdinl@live.com</a> <br/>
-            Github: <a href='https://github.com/ajdinl' target='blank'>https://github.com/ajdinl</a> <br/>
-            Nationality: BiH <br/>
-            Date of birth: 25.01.1988. <br/>
+            <b>First and Surname:</b> Ajdin Lojić <br/>
+            <b>Address:</b> Zagrebačka 55a, 71000 Sarajevo, Bosna i Hercegovina <br/>
+            <b>Phone number:</b> +38761811375 <br/>
+            <b>E-mail:</b> <a href="mailto:ajdinl@live.com">ajdinl@live.com</a> <br/>
+            <b>Github:</b> <a href='https://github.com/ajdinl' target='blank'>https://github.com/ajdinl</a> <br/>
+            <b>Date of birth:</b> 25.01.1988. <br/>
           </div>
           <div className='mb-2'>
-            <b>WORK EXPERIENCE </b><br/>
+            <b>EXPERIENCE </b><br/>
           </div>
-            <div className='mb-1'>
-            <b>Dates: Januar 2015. - today <br/>
-            Name and address of employer: ACCOUNTING d.o.o. Accounting agency. <br/>
-            Type of business or sector: Accounting service,turnover of goods and services <br/>
-            Occupation or Position: Administrative and tehnical assistent </b><br/>
+            <div className='mb-2'>
+            <b>2015. - present <br/>
+            ACCOUNTING d.o.o. Accounting agency. </b><br/>
+            Administrative and technical assistant <br/>
+            
             Main activities and responsibilities: <br/>
                 • Coordinates tasks and execution orders, <br/>
                 • Coordinating and performing administrative tasks, <br/>
                 • Keep records in Excel and creating reports, <br/>
                 • Keeping a budget and cash register table, <br/>
                 • Keeping records, preparing, scheduling and logging incoming and outgoing mail, <br/>
-                • Provide operational support to the team, <br/>
-                • Logistical support, <br/>
-                • Administrative support, <br/>
-                • Organization and coordination of business trips and meetings(reservation of tickets,hotels,rent-a-car), <br/>
+                • Provide operational support to the team, <br/>  
                 • Assistence to superiors in carrying out work activities, <br/>
                 • Creating and posting documentation ( KUF, KIF ). <br/>
           </div>
-          <div className='mb-1'>
-            <b>Dates: 2012. - today <br/>
-            Name and address of employer: UPWORK freelance website <br/>
-            Type of business or sector: Photo editing – Upwork <br/>
-            Occupation or Position: Photographer - editor </b><br/>
-            Main activities and responsibilities:   <br/>
-                • Images basics: pixel concept, image resolution,image size, color systems in graphic image processing, channels and color depth, <br/>
-                • Selection in Photoshop: types, tools, modes and styles of selection, selection masks, selection menu, subsequent modifications to the selection, <br/>
-                • Photo processing: color adjustment, tone, color saturation and brightness, color management, color palettes, photoflow workflow, photo retouching and repair, <br/>
-                • Layers: image layer features, layer palette, layer view, layer groups, layer binding, layer composition, <br/>
-                • Masks: Types and uses of masks, layer mask, text masking, mask clipping, Quick Mask mode, <br/>
-                • Drawing: basics of vector trajectories and points, drawing, drawing shapes, creating your own shapes, <br/>
-                • Painting: fill and border settings, color blending modes, transparency, brush drawing, brush settings, brush management, making your own brush, pattern and gradient, <br/>
-                • Filters: filters gallery, filters in photo processing, special effects and distortion filters, textures, <br/>
-                • Transformations: correct transformations, improper distortion of objects, <br/>
-                • Work phases and automation: history palette, document status recording, action palette, command series, creating, applying and saving your own actions, <br/>
-                • Printing and Web Preparation: Supported Formats, Web Recording, Color Profiles, CMYK Conversion. <br/>
-          </div>
-          <div className='mb-1'>
-            <b>Dates: 2009. – 2016. <br/>
-            Name and address of employer: NEWEVENT d.o.o. Private client <br/>
-            Type of business or sector: Taking photos of events and photo editing <br/>
-            Occupation or Position: Photographer (freelancer) </b><br/>
-            Main activities and responsibilities:   <br/>
+          <div className='mb-4'>
+            <b>2009. – 2016. <br/>
+            NEWEVENT d.o.o. Private client </b><br/>
+            Photographer (freelancer) <br/>
                 • Taking photos of events and places, <br/>
                 • Creating a videos records, <br/>
                 • Design and graphics in Photoshopu, <br/>
                 • Collecting client documentation, <br/>
                 • Reporting on the results of fieldwork, <br/>
                 • Processing of materials, <br/>
-                • Creating a multimedia presentation. <br/>
           </div>
-          <div className='mb-1'>
-            <b>Dates: May 2009. – November 2009. <br/>
-            Name and address of employer: LORA d.o.o. <br/>
-            Type of business or sector: Commercial sector <br/>
-            Occupation or Position: Commercial sales - promoter </b><br/>
-            Main activities and responsibilities:   <br/>
-                • Communication with “big” customers (malls), <br/>
-                • Touring outlets according to a defined schedule, <br/>
-                • Contracting and selling goods to customers - creating orders, <br/>
-                • Maintaining relationships with current customers as well as establishing business relationships with potential new customers, <br/>
-                • Complete control of the existing assortment within the point of sale, implementation of the new assortment and keeping in mind of deadlines, <br/>
-                • Constant concern over the visual identity of the store, <br/>
-                • Positioning of sales assortment in accordance with company guidelines, <br/>
-                • Manage promotional activities and seasonal actions in collaboration with the team, <br/>
-                • Responsibility for planned financial realization as well as payment to customers, <br/>
-                • Taking care of a positive business relationship within the team. <br/>
-          </div>
-          <div className='mt-3 mb-2'>
+          <div className='mt-3 mb-3'>
             <b>EDUCATION </b><br/>
           </div>
           <div>
-            • Dates: 2012 - 2014. <br/>
-            • Faculty of Political Science, University of Sarajevo, Department of communication studies, Business communication, <br/>
-            • Direction of Education: Journalism - Business Communication, <br/>
-            • Qualifications: Bachelor of Business Communication. <br/><br/>
-
-            • Dates: 2010-2011. <br/>
-            • J.U. „Mješovita srednja škola“ Gračanica, <br/>
-            • Direction of Education: Motor vehicle driver. <br/><br/>
-
-            • Dates: 2003-2007. <br/>
-            • J.U. „Željeznički school centar“ Sarajevo and J.U. „Mješovita srednja škola“ Gračanica, <br/>
-            • Direction of Education: Electrical technician. <br/><br/>
+            <b>2012 - 2014. <br/>
+            • Faculty of Political Science, University of Sarajevo </b><br/>
+            • Bachelor of Business Communication. <br/><br/>
+            <b>2003-2007. <br/>
+            • J.U. „Željeznički school centar“ Sarajevo and J.U. „Mješovita srednja škola“ Gračanica </b><br/>
+            • Electrical technician. <br/>
 
           </div>  
           <div className='mt-3 mb-2'>
-            <b>PERSONAL ABILITIES AND COMPETENCES </b><br/>
+            <b>SKILLS </b><br/>
           </div>
           <div>
-            Knowledge of English <br/>
-              • Reading: Very good, <br/>
-              • Writing: Very good, <br/>
-              • Communication: Good. <br/>
+              • Development of interpersonal communication <br/>
+              • Clear and precise communication <br/>
+              • Expressed creativity and self-initiative <br/>
+              • Good sense of setting priorities and organizing work <br/>
           </div>  
           <div className='mt-3 mb-2'>
-            <b>SOFTWARE DEVELOPER SKILLS </b><br/>
+            <b>SOFTWARE SKILLS </b><br/>
           </div>
           <div>
-            • Knowledge od HTML, CSS, Javascript and various frameworks and libraries (React, Router, Redux, Node, Express, Bootstrap), <br/>
-            • Experience with design pattern, frameworks and architecture , <br/>
-            • Knowledge of object-oriented software development methodoloies <br/>
-            • Experience using Git and with version control via Git. <br/>
+            • MS Office &emsp;&emsp;&emsp;&emsp;&emsp;&ensp;&nbsp;<i class="fas fa-circle"></i> <i class="fas fa-circle"></i> <i class="fas fa-circle"></i> <i class="fas fa-circle"></i> <i class="fas fa-circle"></i> <i class="fas fa-circle"></i><br/>
+            • Photoshop/Lightroom &ensp;<i class="fas fa-circle"></i> <i class="fas fa-circle"></i> <i class="fas fa-circle"></i> <i class="fas fa-circle"></i> <i class="fas fa-circle"></i> <i class="fas fa-circle"></i><br/>
+            • HTML/CSS &ensp;&emsp;&emsp;&emsp;&emsp;&ensp;&nbsp;<i class="fas fa-circle"></i> <i class="fas fa-circle"></i> <i class="fas fa-circle"></i> <i class="fas fa-circle"></i> <i class="far fa-circle"></i> <i class="far fa-circle"></i><br/>
+            • NodeJS &emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&ensp;&nbsp;<i class="fas fa-circle"></i> <i class="fas fa-circle"></i> <i class="fas fa-circle"></i> <i class="fas fa-circle"></i> <i class="far fa-circle"></i> <i class="far fa-circle"></i><br/>
+            • ReactJS &ensp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;<i class="fas fa-circle"></i> <i class="fas fa-circle"></i> <i class="fas fa-circle"></i> <i class="fas fa-circle"></i> <i class="far fa-circle"></i> <i class="far fa-circle"></i><br/>
+            • SQL &emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;<i class="fas fa-circle"></i> <i class="fas fa-circle"></i> <i class="fas fa-circle"></i> <i class="far fa-circle"></i> <i class="far fa-circle"></i> <i class="far fa-circle"></i><br/>
           </div>    
           <div className='mt-3 mb-2'>  
-            <b>SOCIAL ABILITIES AND COMPETENCES </b><br/>
+            <b>HOBBY </b><br/>
           </div>
           <div>
-            Development of interpersonal communication, adaptability to teamwork, skill of clear and precise communication. Sociality and willingness to accept criticism and suggestions, as well as to support and give advice. Driven by the desire to constantly acquire new knowledge and skills. <br/>
+          <i class="fas fa-motorcycle fa-3x"></i> &emsp;&emsp; <i class="fas fa-camera fa-3x"></i> &emsp;&emsp;&emsp; <i class="fas fa-chart-line fa-3x"></i><br/>
+          <b>Motorbikes</b> &ensp; <b>Photography</b> &ensp; <b>Crypto Currency</b>
           </div>
-          <div className='mt-3 mb-2'>
-            <b>ORGANIZATIONAL ABILITIES AND COMPETENCES </b><br/>
-          </div>
-          <div>
-            Expressed creativity and self-initiative, inventiveness in the performance of work tasks, responsibility for personal performance of work and work tasks, openness to new ideas and innovations, a good sense of setting priorities and organizing work, consistently performing work tasks and meeting deadlines, working on a multitasking principle and working under pressure. <br/>
-          </div>
-          <div className='mt-3 mb-2'>
-            <b>TECHNICAL ABILITIES AND COMPETENCES </b><br/>
+          <div className='mt-4 mb-2'>
+            <b>LANGUAGES </b><br/>
           </div>
           <div>
-            Excellent computer skills. Operating systems Windows, Linux and use of MS Office (Word, Excel, Powerpoint), excellent knowledge and use of the internet and internet browsers. <br/>
-            Excellent knowledge of all computer components (Hardware and Software). Good knowledge of working with programming languages (javascript / node and javascript / react) and excellent knowledge of working in graphic design applications such as Photoshop and Lightroom. <br/>
+            English &ensp;<i class="fas fa-circle"></i> <i class="fas fa-circle"></i> <i class="fas fa-circle"></i> <i class="fas fa-circle"></i> <i class="far fa-circle"></i> <i class="far fa-circle"></i><br/>
           </div>
-          <div className='mt-3 mb-2'>
-            <b>ADDITIONAL CERTIFICATES </b><br/>
+          <div className='mt-4 mb-2'>
+            <b>FREELANCING </b>
           </div>
-          <div className='mb-4'>
-            Passed driving test of category A and B (KV state exam). <br/>
-            Windows and MS Office course completed (Year : 2010.). <br/>
-          </div>
+          <div>
+          <b>UPWORK freelance website</b> <br/>
+          • Images basics: pixel concept, image resolution,image size, color systems in graphic image processing, channels and color depth,<br/>
+          • Selection in Photoshop: types, tools, modes and styles of selection, selection masks, selection menu, subsequent modifications to the selection,<br/>
+          • Photo processing: color adjustment, tone, color saturation and brightness, color management, color palettes, photoflow workflow, photo retouching and repair,<br/>
+          • Layers: image layer features, layer palette, layer view, layer groups, layer binding, layer composition,<br/>
+          • Masks: Types and uses of masks, layer mask, text masking, mask clipping, Quick Mask mode,<br/>
+          • Drawing: basics of vector trajectories and points, drawing, drawing shapes, creating your own shapes,<br/>
+          • Painting: fill and border settings, color blending modes, transparency, brush drawing, brush settings, brush management, making your own brush, pattern and gradient,<br/>
+          • Filters: filters gallery, filters in photo processing, special effects and distortion filters, textures,<br/>
+          • Transformations: correct transformations, improper distortion of objects,<br/>
+          • Work phases and automation: history palette, document status recording, action palette, command series, creating, applying and saving your own actions,<br/>
+          • Printing and Web Preparation: Supported Formats, Web Recording, Color Profiles, CMYK Conversion.<br/><br/>
+          </div>          
         </div>
       </div>
     </div>
